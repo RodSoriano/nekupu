4d03665ebe1420eac3e4b2a11ad1ec597a436d07
Bryan Portillo <bportillo701@gmail.com>
2024-09-28 01:27:38 -0600

    ✨ Added ChatRoom module

---
+++ app/api/src/chat-room/entities/chat-room.entity.ts
@@ -0,0 +1,14 @@
+import { Entity, OneToMany, OneToOne } from 'typeorm';
+
+import { BaseEntity } from '../../common/entities/base-entity.entity';
+import { Message } from '../../message/entities/message.entity';
+import { User } from '../../user/entities/user.entity';
+
+@Entity()
+export class ChatRoom extends BaseEntity {
+  @OneToMany(() => Message, (message) => message.chatRoom)
+  messages: Message[];
+
+  @OneToOne(() => User, (user) => user.chatRoom)
+  user: User;
+}
